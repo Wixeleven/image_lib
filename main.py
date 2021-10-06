0e89a48ca33e6081edaca9c9724f3c9bcfc3c7c1
Wixeleven <80391055+Wixeleven@users.noreply.github.com>
2021-10-06 10:13:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from PIL import Image, ImageFilter
+im = Image.open("bilde.jpg")
+print(im.format, im.size, im.mode)
+#im.show()
+izmers = (132, 84 )
+im.thumbnail(izmers)
+im.save("bilde-maza.jpg", im.format)
+#im.show()
+im = im.rotate(132)
+#im.show()
+r, g, b = im.split()
+
+im = Image.merge("RGB", (g,r,b))
+#im.show()
+im = im.filter(ImageFilter.CONTOUR)
+im.show()
+
+
+
+
